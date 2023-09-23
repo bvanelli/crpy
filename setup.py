b71a55d51fea1cef6fb83bac8efe2e8225c643a8
Brunno Vanelli <brunnovanelli@gmail.com>
2023-09-23 20:27:39 +0200

    build: Update urls, change description, bump version.

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name="crpy",
+    version="0.0.1",
     packages=find_packages(),
-    description="Simple and straight forward wrapper for container registry API.",
+    description="Simple and straight forward async library for interacting with container registry API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Brunno Vanelli",
     author_email="brunnovanelli@gmail.com",
-    url="https://github.com/bvanelli/docker-pull-push",
+    url="https://github.com/bvanelli/crpy",
     zip_safe=False,
     project_urls={
-        "Issues": "https://github.com/bvanelli/docker-pull-push/issues",
+        "Issues": "https://github.com/bvanelli/crpy/issues",
     },
     entry_points="""
       [console_scripts]
