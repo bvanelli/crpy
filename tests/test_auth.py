c8ed2732591dffbfa6939e2971910df00cde123f
Brunno Vanelli <brunnovanelli@gmail.com>
2025-07-31 21:11:14 +0200

    fix: Configure asyncio for tests.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,6 @@
-import pytest
-
 from crpy.registry import RegistryInfo
 
 
-@pytest.mark.asyncio
 async def test_auth():
     registry = RegistryInfo.from_url("alpine")
     token = await registry.auth()
